80c751ef1482d285273da85cb534a82516f6dcb7
AkxshHans <162800417+AkxshHans@users.noreply.github.com>
2024-03-25 10:50:33 +0530

    added index.html , style.css , script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+gsap.from("#navpart1 ,  #navpart2 h5 , #navpart3 ",    {
+y : -100,
+duration : 1,
+delay : 1,
+opacity : 0.1,
+Stagger : 1  ,
+
+
+
+})
+
+gsap.from(" #img1 , #img2 ",    {
+    y : -100,
+    duration : 1,
+    delay : 1.6,
+    opacity : 0,
+    Stagger : 1  ,
+    
+    
+    
+    })
+
+
+    gsap.from(" #DISCIPLINE , #EVERY, #EASY ",    {
+        y : -100,
+        duration : 1,
+        delay : 1.6,
+        opacity : 0,
+        Stagger : 1  ,
+        
+        
+        
+        })
+
+
+
+        gsap.from(" i",    {
+            y : -100,
+            duration : 1,
+            delay : 2.1,
+            opacity : 0,
+            Stagger : 1  ,
+            
+            
+            
+            })
+
+
+
+
+
+
+
+
+
+           
+
+
